bc19a11c126cffd9aec9f01df61830cb727920f1
Jack Knotts <redflare1234@gmail.com>
2024-03-26 09:33:27 -0400

    Added the demo version to the source so we have an structure to start out with.

---
+++ gs-handling-form-submission-main/complete/src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -0,0 +1,33 @@
+package com.example.handlingformsubmission;
+
+public class Greeting {
+
+	private long id;
+	private String content;
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
