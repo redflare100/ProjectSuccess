edfc9bd7270c4551a4e757ce1d18c4de5fbc33e2
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-03-26 17:09:16 -0400

    added imports and lombok to project so that teacher class functions

--- teacher/src/main/java/com/projectsuccess/teacherportal/Teacher.java
+++ teacher/src/main/java/com/projectsuccess/teacherportal/Teacher.java
@@ -1,7 +1,12 @@
 package com.projectsuccess.teacherportal;
 
-@Setter
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
 @Getter
+@Setter
 public class Teacher {
     public int teacherId;
     private String firstName;
--- teacher/src/test/java/com/projectsuccess/teacher/TeacherApplicationTests.java
+++ teacher/src/test/java/com/projectsuccess/teacher/TeacherApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TeacherApplicationTests {
+class TeacherPortalTests {
 
 	@Test
 	void contextLoads() {
