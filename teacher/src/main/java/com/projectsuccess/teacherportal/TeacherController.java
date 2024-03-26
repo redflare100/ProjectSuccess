a079e90f7243a312f93e117532aa6eb5596fc24c
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-03-26 18:42:17 -0400

    fixed dependencies so controller imports would work

--- teacher/src/main/java/com/projectsuccess/teacherportal/TeacherController.java
+++ teacher/src/main/java/com/projectsuccess/teacherportal/TeacherController.java
@@ -2,12 +2,14 @@ package com.projectsuccess.teacherportal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class TeacherController {
 
     @GetMapping("/assignment")
-        public String getAssignmentDetails(@RequestParam int assignmentId) {
+    public String getAssignmentDetails(@RequestParam int assignmentId) {
         return "" + assignmentId;
     }
 
