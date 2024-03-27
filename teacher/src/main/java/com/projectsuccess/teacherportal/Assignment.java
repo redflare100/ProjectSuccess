acd361d69ec5b5a5bcf67ed4d8c2fd801d743fa8
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-03-26 22:24:19 -0400

    added hardcoded data

--- teacher/src/main/java/com/projectsuccess/teacherportal/Assignment.java
+++ teacher/src/main/java/com/projectsuccess/teacherportal/Assignment.java
@@ -1,5 +1,14 @@
 package com.projectsuccess.teacherportal;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.expression.spel.ast.Assign;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Setter
 @Getter
 public class Assignment {
@@ -8,4 +17,25 @@ public class Assignment {
     private LocalDateTime dueDate;
     private List<String> requirements;
 
+    public Assignment() { }
+
+    public Assignment(int assignmentId, String name, LocalDateTime dueDate, List<String> requirements) {
+        this.assignmentId = assignmentId;
+        this.name = name;
+        this.dueDate = dueDate;
+        this.requirements = requirements;
+    }
+
+    // hard coded assignment lists
+    public static List<Assignment> chemistry101Assignments = new ArrayList<>(Arrays.asList(
+        // empty for now
+    ));
+
+    public static List<Assignment> physics212Assignments = new ArrayList<>(Arrays.asList(
+            // empty for now
+    ));
+
+    public static List<Assignment> history112Assignments = new ArrayList<>(Arrays.asList(
+            // empty for now
+    ));
 }
--- teacher/src/main/java/com/projectsuccess/teacherportal/Teacher.java
+++ teacher/src/main/java/com/projectsuccess/teacherportal/Teacher.java
@@ -3,6 +3,8 @@ package com.projectsuccess.teacherportal;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -13,13 +15,39 @@ public class Teacher {
     private String lastName;
     private List<Course> courses;
 
+    public Teacher() {
+
+    }
+
+    public Teacher(int teacherId, String firstName, String lastName, List<Course> courses) {
+        this.teacherId = teacherId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courses = courses;
+    }
+
+
     public static void main(String[] args) {
 
-        Teacher[] teachers = new Teacher[5];
-        for (int i = 0; i < teachers.length; i++) {
-            teachers[i] = new Teacher();
-            teachers[i].setTeacherId(i);
-        }
+        // hard coded data
+        Teacher stemTeacher = new Teacher(
+                0,
+                "Walter",
+                "White",
+                new ArrayList<>(Arrays.asList(
+                    Course.chemistry101,
+                    Course.physics212
+                ))
+        );
+
+        Teacher historyTeacher = new Teacher(
+                1,
+                "Simon",
+                "Bolivar",
+                new ArrayList<>(Arrays.asList(
+                    Course.history112
+                ))
+        );
 
     }
 
