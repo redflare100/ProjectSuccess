c669a0daa211c90442d6e421f14b5cee3dbbbf52
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-04-02 21:51:17 -0400

    rewrote HTML page to explicitly state assignment details. added missingAssignment page for when assignmentId is not found

--- teacher/src/main/java/com/projectsuccess/teacherportal/TeacherController.java
+++ teacher/src/main/java/com/projectsuccess/teacherportal/TeacherController.java
@@ -11,12 +11,19 @@ public class TeacherController {
 
     @GetMapping("/assignment")
     public String assignmentDetails(
-            @RequestParam(name = "assignmentId", defaultValue = "0") int assignmentId,
+            @RequestParam(name = "assignmentId", defaultValue = "0", required = false) int assignmentId,
             Model model
     ) {
         Assignment assignment = TeacherService.retrieveAssignmentDetails(assignmentId);
+        if (assignment == null) {
+            return "missingAssignment";
+        }
 
-        model.addAttribute("assignmentId", assignmentId);
+        model.addAttribute("name", assignment.getName());
+        model.addAttribute("id", assignment.getAssignmentId());
+        model.addAttribute("dueDate", assignment.getDueDate());
+        model.addAttribute("requirements", assignment.getRequirements());
+
         return "assignmentDetails";
     }
 
